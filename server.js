67629653e71946fd2507620c366ca64cf8325e7f
AZIKDEV <azikdev23@gmail.com>
2024-10-20 19:36:25 +0500

    Update server.js

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ const mineflayer = require("mineflayer");
 const bodyParser = require("body-parser");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 let bot;
 
