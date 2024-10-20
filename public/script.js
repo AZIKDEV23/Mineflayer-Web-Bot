4ed48b31970a93d77462aa2d7d663e713f2d06e2
AZIKDEV <azikdev23@gmail.com>
2024-10-20 15:00:08 +0500

    js

--- public/script.js
+++ public/script.js
@@ -1,15 +1,47 @@
-document.getElementById("loginForm").addEventListener("submit", function (e) {
-  e.preventDefault();
+let bot;
 
-  const username = document.getElementById("username").value;
-  const server = document.getElementById("server").value;
-  const port = document.getElementById("port").value;
+document
+  .getElementById("start")
+  .addEventListener("click", async function (event) {
+    event.preventDefault();
 
-  fetch("/start-bot", {
-    method: "POST",
-    headers: {
-      "Content-Type": "application/json",
-    },
-    body: JSON.stringify({ username, server, port }),
+    const host = document.getElementById("host").value;
+    const port = document.getElementById("port").value;
+    const username = document.getElementById("username").value;
+    const version = document.getElementById("version").value;
+
+    const response = await fetch("/connect", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+      body: JSON.stringify({ host, port, username, version }),
+    });
+
+    const result = await response.json();
+    console.log(result.message);
+
+    if (result.success) {
+      bot = true; // Bot muvaffaqiyatli ulanishni bildiradi
+      document.querySelector(".color").style.color = "green"; // Tugma rangini yashilga o'zgartirish
+    }
   });
+
+document.getElementById("quit").addEventListener("click", async function () {
+  if (bot) {
+    const response = await fetch("/disconnect", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+
+    const result = await response.json();
+    console.log(result.message);
+
+    bot = false; // Bot ulanishni tugatganini bildiradi
+    document.querySelector(".color").style.color = "red"; // Tugma rangini qizilga o'zgartirish
+  } else {
+    console.log("Bot hali ulanishni boshlamagan.");
+  }
 });
